2e9ec33c5cb21714a141214d984efb413f844ab6
jadilovic <adilovic79@yahoo.com>
2023-01-19 12:04:51 +0100

    first commit

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,19 +1,37 @@
-const getElement = (selector) => {
-  const element = document.querySelector(selector)
+// const getElement = (selector) => {
+//   const element = document.querySelector(selector)
 
-  if (element) return element
-  throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
+//   if (element) return element
+//   throw Error(
+//     `Please double check your class names, there is no ${selector} class`
+//   )
+// }
+
+// const links = getElement('.nav-links')
+// const navBtnDOM = getElement('.nav-btn')
+
+// navBtnDOM.addEventListener('click', () => {
+//   links.classList.toggle('show-links')
+// })
+
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) {
+    return element;
+  } else {
+    throw Error(
+      `Please double checkout your class name, there is no ${selector} class`
+    )
+  }
 }
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
+const links = getElement(".nav-links");
+const navBtn = getElement(".nav-btn");
 
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
+navBtn.addEventListener('click', () => {
+  links.classList.toggle("show-links")
 })
 
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+const element = document.getElementById("date");
+const year = new Date().getFullYear();
+element.textContent = year;
